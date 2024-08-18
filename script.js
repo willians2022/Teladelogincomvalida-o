ce2fb9f72ebef27dececf5048fcb8aae6d1c0e1d
willians2022 <willians_zno@hotmail.com>
2024-08-18 20:58:01 -0300

    teladelogimcomvalidacao

---
+++ script.js
@@ -0,0 +1,11 @@
+function logar() {
+    var login = document.getElementById("login").value;
+    var senha = document.getElementById("senha").value;
+
+    if (login == "admin" && senha == "admin") {
+        alert("sucesso");
+        location.href = "https://www.microsoft.com/pt-br/microsoft-365/outlook/email-and-calendar-software-microsoft-outlook?deeplink=%2fowa%2f%3frealm%3dhotmail.com&sdf=0"
+    } else {
+        alert("usuario  e senha  incorreta ");
+    }
+}
